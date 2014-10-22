bec821d059151fa1eb72faf68eaa842918b219a0
coolllll <best22772@gmail.com>
2014-10-22 21:07:48 +0700

    Update regis.php

--- php/regis.php
+++ php/regis.php
@@ -1,18 +1,20 @@
 <?php
 	include "connect.php";
 	$conn = connToServ();
-	$name = $_POST["user"];
+	$user = $_POST["user"];
 	$pass = $_POST["pass"];
 	$toret = array();
-	//$toret['stat']=0;
+	//echo $name;
+	//$toret['stat']=1;
 	//return json_encode($toret);
-	if(mysqli_query($conn,"SELECT * FROM loginsys WHERE user='$user'"))
+	$result = mysqli_query($conn,"SELECT * FROM loginsys WHERE user='$user'");
+	if(mysqli_fetch_array($result))
 	{
 		$toret['stat']=0;
 	}
 	else 
 	{
-		$sql = "INSERT INTO loginsys(user,pass) VALUES ('$user','MD5($pass)')";
+		$sql = "INSERT INTO loginsys(user,pass) VALUES ('$user',MD5('$pass'))";
 		mysqli_query($conn, $sql);
 		$toret['stat']=1;
 	}
