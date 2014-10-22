98fd852248c00c9bfc9f72846b975ed9151652e3
coolllll <best22772@gmail.com>
2014-10-22 20:23:25 +0700

    Create regis.php

---
+++ php/regis.php
@@ -0,0 +1,20 @@
+<?php
+	include "connect.php";
+	$conn = connToServ();
+	$name = $_POST("user");
+	$pass = $_POST("pass");
+	$toret[];
+	$toret['stat']=0;
+	return json_encode($toret);
+	if(mysqli_query($conn,"SELECT * FROM loginsys WHERE user='$user'"))
+	{
+		$toret['stat']=0;
+	}
+	else 
+	{
+		$sql = "INSERT INTO loginsys(user,pass) VALUE ('$user','MD5($pass)')";
+		mysqli_query($conn, $sql);
+		$toret['stat']=1;
+	}
+	return json_encode($toret);
+?>
